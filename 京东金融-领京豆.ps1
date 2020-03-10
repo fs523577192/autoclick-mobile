8b4dda067b945e2d68aa831fd51b570714c8b7f7
Firas <448445223@qq.com>
2020-03-10 14:37:11 +0800

    Add doing viewing tasks in JD Finance

--- 京东金融-领京豆.ps1
+++ 京东金融-领京豆.ps1
@@ -57,8 +57,8 @@ function swipe {
     .\adb shell input swipe 536 1592 543 258 1000
     .\adb shell input swipe 536 1592 543 1400 1000
 }
-
-for ($private:i = 0; $private:i -lt 6; $private:i += 1) {
+# for ($private:j = 0; $private:j -lt 3; $private:j += 1)
+for ($private:i = 0; $private:i -lt 3; $private:i += 1) {
     .\adb shell screencap -p /storage/self/primary/DCIM/Screenshots/adb_cap.png
     .\adb pull /storage/self/primary/DCIM/Screenshots/adb_cap.png
     $private:result=java -cp F:\java\screenShotAnalyzer\out\production\screenShotAnalyzer\ org.firas.tool.ssa.Main  match  D:\adb\adb_cap.png  D:\adb\jdf_to_view1.png 807 240 936 1308 | findstr Matched
@@ -76,4 +76,50 @@ for ($private:i = 0; $private:i -lt 6; $private:i += 1) {
         swipe
         Start-Sleep -Seconds 2
     }
+}
+
+# 请先准备好每日签到页面
+for ($private:i = 0; $private:i -lt 4; $private:i += 1) {
+    .\adb shell input swipe 855 1113 521 1113 1000
+    Start-Sleep -Seconds 2
+    .\adb shell input tap 855 1113
+    echo 去浏览
+    waitFinish
+    .\adb shell input keyevent 4
+    echo 返回
+    Start-Sleep -Seconds 5
+}
+
+# 请先准备好“金果摇钱树”任务页面
+for ($private:i = 0; $private:i -lt 6; $private:i += 1) {
+    .\adb shell input tap 908 1347
+    echo 去浏览
+    Start-Sleep -Seconds 4
+
+    .\adb shell input keyevent 4
+    echo 返回
+    Start-Sleep -Seconds 6
+
+    .\adb shell screencap -p /storage/self/primary/DCIM/Screenshots/adb_cap.png
+    .\adb pull /storage/self/primary/DCIM/Screenshots/adb_cap.png
+    $private:result=java -cp F:\java\screenShotAnalyzer\out\production\screenShotAnalyzer\ org.firas.tool.ssa.Main  match  D:\adb\adb_cap.png  D:\adb\jdf_fruit_task.png 30 1066 196 1428 | findstr Matched
+    echo $private:result
+    if ( "$private:result" -eq "" ) {
+        .\adb shell input keyevent 4
+        echo 返回
+        Start-Sleep -Seconds 5
+    }
+    .\adb shell screencap -p /storage/self/primary/DCIM/Screenshots/adb_cap.png
+    .\adb pull /storage/self/primary/DCIM/Screenshots/adb_cap.png
+    $private:result=java -cp F:\java\screenShotAnalyzer\out\production\screenShotAnalyzer\ org.firas.tool.ssa.Main  match  D:\adb\adb_cap.png  D:\adb\jdf_fruit_task.png 30 1366 196 1428 | findstr Matched
+    echo $private:result
+    if ( "$private:result" -ne "" ) {
+        echo 无法找到任务按钮
+        break
+    }
+
+    Start-Sleep -Seconds 5
+    .\adb shell input tap 103 1153
+    echo 任务
+    Start-Sleep -Seconds 4
 }
