9e4401cd0cdde9103df3b8f2a4c346860d1e12f4
Firas <448445223@qq.com>
2022-11-09 12:27:16 +0800

    支付宝会员和更新美团-天天抽奖

---
+++ 支付宝会员.ps1
@@ -0,0 +1,31 @@
+﻿cd D:\adb
+.\adb devices
+
+function goBack {
+    .\adb shell input keyevent 4
+    echo 返回
+    Start-Sleep -Seconds 2
+}
+function swipe {
+    Start-Sleep -Seconds 1
+    .\adb shell input swipe 536 1600 543 1400 500
+}
+
+for ($private:i = 0; $private:i -lt 6; $private:i += 1) {
+    .\adb shell screencap -p /storage/self/primary/Pictures/Screenshots/adb_cap.png
+    .\adb pull /storage/self/primary/Pictures/Screenshots/adb_cap.png
+    $private:result=java -cp F:\java\screenShotAnalyzer\out\production\screenShotAnalyzer\ org.firas.tool.ssa.Main  match  D:\adb\adb_cap.png  D:\adb\zfb_hongbao.png 198 1149 741 2190 | findstr Matched
+    .\adb shell input tap 540 2353
+    echo $private:result
+    if ( "$private:result" -ne "" ) {
+        $private:result = "$private:result".Split(" ")
+        $private:x = [int]$private:result[1] + 700
+        $private:y = [int]$private:result[2] + 100
+        .\adb shell input tap $private:x $private:y
+        echo 逛红包
+        swipe
+        Start-Sleep -Seconds 17
+        goBack
+    }
+}
+    
--- 美团-天天抽奖.ps1
+++ 美团-天天抽奖.ps1
@@ -1,14 +1,14 @@
 ﻿cd D:\adb
 .\adb devices
 
-for ($private:i = 0; $private:i -lt 10; $private:i += 1) {
+for ($private:i = 0; $private:i -lt 12; $private:i += 1) {
     .\adb shell input tap 543 1254
     echo "抽奖"
     Start-Sleep -Seconds 3
 
     .\adb shell screencap -p /storage/self/primary/Pictures/Screenshots/adb_cap.png
     .\adb pull /storage/self/primary/Pictures/Screenshots/adb_cap.png
-    $private:result=java -cp F:\java\screenShotAnalyzer\out\production\screenShotAnalyzer\ org.firas.tool.ssa.Main  match  D:\adb\adb_cap.png  D:\adb\MeituanFruit\CloseQuChouJiang.png 846 765 907 823 | findstr Matched
+    $private:result=java -cp F:\java\screenShotAnalyzer\out\production\screenShotAnalyzer\ org.firas.tool.ssa.Main  match  D:\adb\adb_cap.png  D:\adb\MeituanFruit\CloseQuChouJiang.png 844 765 907 823 | findstr Matched
     echo $private:result
     if ( "$private:result" -ne "" ) {
         $private:result = "$private:result".Split(" ")
